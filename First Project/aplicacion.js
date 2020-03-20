5a1a93e083137ab67b22da46e72898253a954f00
Rene Zelada <razp92@gmail.com>
2020-03-20 13:17:18 -0600

    logs

--- First Project/aplicacion.js
+++ First Project/aplicacion.js
@@ -1,3 +1,4 @@
-console.log("Hola Bienvenidos");
-console.log("Este es desde TypeScript");
-console.log("Hola 123");
+console.log('Hola Bienvenidos');
+console.log('Este es desde TypeScript');
+console.log('Hola 123');
+console.warn('esto es una advertencia');
