382a937f8e9c9103c3c43a52be8a25431bd697dd
Rene Zelada <razp92@gmail.com>
2020-03-23 12:26:47 -0600

    Components

--- holaMundo/src/app/titulos/titulos.component.ts
+++ holaMundo/src/app/titulos/titulos.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./titulos.component.scss']
 })
 export class TitulosComponent implements OnInit {
+nombre:string = "Maria"
+apellido:string ="Perez"
 
-  constructor() { }
+
+alumno:any ={
+  nombre:"carlos",
+  apellido:"pineda",
+  edad:21
+}
+imagen:string="https://www.paredro.com/wp-content/uploads/2019/05/Facebook-Logo.jpg"
+inputNuevo:string ="Hola soy un nuevo input"
+
+correo:string = ''
+password:string =''
+constructor() { }
 
   ngOnInit() {
+  }
+
+  ingresar(){
+    console.log(this.correo)
+    console.log(this.password)
   }
 
 }
